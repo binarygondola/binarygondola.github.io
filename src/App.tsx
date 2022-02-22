0d045c45825c54bdefd126ac30045a7203bb9cbe
Grzegorz Sołdatowski <grzegorz.soldatowski@itwl.pl>
2022-02-22 11:28:04 +0100

    builded & published

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/Aapp.tsx</code> and save to reload.
+          Edit <code>src/Aapp.tsx</code> and saveasdsad to reload.
         </p>
         <p>
           Thats awesome
